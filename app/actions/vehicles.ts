5611f9c81a61b10d477530cc6e5feff90ea4a6d6
Jordan Roberts <jrobertswebdev@gmail.com>
2024-07-12 09:34:34 -0500

    switch to action from api route

--- app/actions/vehicles.ts
+++ app/actions/vehicles.ts
@@ -3,13 +3,19 @@ import { Window } from '@/lib/shopify/types'
 import { Vehicle } from '@/lib/shopify/types'
 const API_URL = 'https://localhost:8888'
 export const addNewVehicle = async (data: { vehicle: Vehicle; window: Window }) => {
- const response = fetch(`/api/vehicle`, {
-  method: 'POST',
-  headers: {
-   'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
- })
- console.log('response:', response)
- return await response
+ console.log('data:', data)
+ try {
+  const response = fetch(`${API_URL}/save`, {
+   method: 'POST',
+   headers: {
+    'Content-Type': 'application/json',
+   },
+   body: JSON.stringify(data),
+  })
+  console.log('response:', response)
+  return { success: true, data: (await response).json() }
+ } catch (error) {
+  console.log('error:', error)
+  return error
+ }
 }
--- app/content/sortOptions.ts
+++ app/content/sortOptions.ts
@@ -1,8 +1,8 @@
 export const sortOptions = [
  { id: 'az', label: 'A-Z', value: 'TITLE', reverse: false },
  { id: 'za', label: 'Z-A', value: 'TITLE', reverse: true },
- { id: 'new', label: 'Newest', value: 'CREATED', reverse: false },
- { id: 'old', label: 'Oldest', value: 'CREATED', reverse: true },
+ { id: 'new', label: 'Newest', value: 'CREATED', reverse: true },
+ { id: 'old', label: 'Oldest', value: 'CREATED', reverse: false },
 ] as const
 
 export type SortOption = {
