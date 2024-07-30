3c78faaca7bed41397dbcbdc7b3813720f5de448
Jordan Roberts <jrobertswebdev@gmail.com>
2024-07-30 12:13:20 -0500

    add reverse proxy for fonz

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
-
+const fonzUrl = process.env.NEXT_PUBLIC_FONZ_URL
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+ async rewrites() {
+  return [
+   {
+    source: '/fonz/:match*',
+    destination: `${fonzUrl}/:match*`,
+   },
+  ]
+ },
  transpilePackages: ['jotai-devtools'],
  experimental: {
   serverActions: {
