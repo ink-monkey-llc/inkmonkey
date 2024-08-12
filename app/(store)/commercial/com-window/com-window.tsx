503e5a1633cfa7156e9c58b973eea650c6c59ee5
Jordan Roberts <jrobertswebdev@gmail.com>
2024-08-12 11:39:37 -0500

    adjust commercial carousel button on mobile

--- app/(store)/commercial/com-window/com-window.tsx
+++ app/(store)/commercial/com-window/com-window.tsx
@@ -5,7 +5,7 @@ import ComContent from './com-content'
 
 function ComWindow() {
  return (
-  <div className='w-full flex gap-8 pt-8 bg-com-bg  bg-cover pb-12 border-t-8 border-accent-tr relative '>
+  <div className='w-full flex gap-8 pt-8 bg-com-bg  bg-cover pb-12 border-t-8 border-accent-tr relative  '>
    <ComCarousel />
    <ComContent />
   </div>
