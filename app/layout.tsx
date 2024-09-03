4e38078565829792f06bea2f5ac61cecc9875231
Jordan Roberts <jrobertswebdev@gmail.com>
2024-09-03 10:27:18 -0500

    update meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
  metadataBase: new URL(metaBase),
  title: 'Ink Monkey',
  description:
-  'Discover the ultimate destination for all your decal needs at Ink Monkey! Specializing in an extensive range of decals. Design your own decal with our powerful AI design tool "Fonzai", or choose from our countless in-house designs. From compact laptop stickers to full wall graphics to vehicle back window graphics made of perforated vinyl, we have everything!',
+  'Discover the ultimate destination for all your sticker and decal needs at Ink Monkey! Specializing in an extensive range of stickers and decals. Design your own decal with our powerful AI design tool "Fonzai", or choose from our countless in-house designs. Get laptops stickers, water bottle stickers, even full wall graphics and vehicle back window graphics made of perforated vinyl, we have everything!',
 }
 
 export default function RootLayout({
