37744d88b3bb26691d9a6f553d6047955d95b4a3
Jordan Roberts <jrobertswebdev@gmail.com>
2024-09-09 15:41:19 -0500

    change git repo to ink monkey org

--- app/(store)/reader/page.tsx
+++ app/(store)/reader/page.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import Upload from './upload'
 import List from './list'
 
 const App: React.FC = () => {
  return (
   <div className='items-center flex flex-col gap-4 max-w-[600px] m-auto py-8'>
-   {/* <Upload> */}
    <h2>Survey Results:</h2>
    <List />
-   {/* </Upload> */}
   </div>
  )
 }
