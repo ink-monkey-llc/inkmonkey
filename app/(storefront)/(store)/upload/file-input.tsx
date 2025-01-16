ab037b079bd8f885931a2a49341d9682fc125fd7
Jordan Roberts <jrobertswebdev@gmail.com>
2025-01-16 10:56:58 -0600

    change file input on upload-image to dnd from vanilla file input

--- app/(storefront)/(store)/upload/file-input.tsx
+++ app/(storefront)/(store)/upload/file-input.tsx
@@ -1,9 +1,10 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { uploadCustomDesign } from '@/app/actions/images'
 import { ErrorToast } from '@/app/toast/error'
-import Disclaimer from './disclaimer'
+import { UploadIcon } from '@/app/icons/upload'
+import { cn } from '@/utils/cn'
 
 function FileInput({
   setImageData,
@@ -13,8 +14,12 @@ function FileInput({
   imageData: { publicID: string; url: string; secure_url: string }
 }) {
   const [uploading, setUploading] = useState(false)
-
   const [selected, setSelected] = useState(false)
+
+  const fileInputRef = useRef<HTMLInputElement>(null)
+  const fileTypes = ['jpg', 'jpeg', 'png', 'svg', 'bmp', 'tiff', 'webp']
+
+  const typeString = fileTypes.map((ext) => `.${ext}`).join(",");
 
   async function generateDataUrl(file: File): Promise<string> {
     return new Promise((resolve, reject) => {
@@ -29,40 +34,81 @@ function FileInput({
     })
   }
 
+  const handleButtonClick = () => {
+    fileInputRef.current?.click()
+  }
+
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files
-    if (files && files.length > 0) {
-      const dataUrl = await generateDataUrl(files[0])
-      const res = await uploadCustomDesign({ dataUrl, fileName: files[0].name, fileSize: files[0].size })
-      if (res.error) {
+    if (!files || files.length < 1) {
+      ErrorToast({ msg: 'Please select a file' })
+      return
+    }
+    if (files.length > 1) {
+      ErrorToast({ msg: 'Please select only one file' })
+      return
+    }
+    handleUpload(files)
+  }
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const droppedFiles = Array.from(e.dataTransfer.files);
+    handleUpload(droppedFiles);
+  };
+
+  const handleUpload = async (files: File[] | FileList) => {
+    setUploading(true)
+    const dataUrl = await generateDataUrl(files[0])
+    const res = await uploadCustomDesign({ dataUrl, fileName: files[0].name, fileSize: files[0].size })
+    if (res.error) {
+      ErrorToast({ msg: 'Image upload failed, please try again' })
+      console.log('error:', res.error)
+      setUploading(false)
+      return { status: 'error' }
+    } else {
+      if (!res.imgData) {
         ErrorToast({ msg: 'Image upload failed, please try again' })
-        console.log('error:', res.error)
-        setUploading(false)
         return { status: 'error' }
-      } else {
-        if (!res.imgData) return { status: 'error' }
-        setImageData(res.imgData)
-        console.log('success:', res.imgData)
-        setUploading(false)
-        setSelected(true)
-        return { status: 'success', imgData: res.imgData }
       }
+      setImageData(res.imgData)
+      console.log('success:', res.imgData)
+      setUploading(false)
+      setSelected(true)
+      return { status: 'success', imgData: res.imgData }
     }
   }
+
+
   return (
-    <div className='px-2 md:w-2/3'>
-      {!selected && <Disclaimer />}
-      <label htmlFor="upload" className="text-sm">
-       {!selected ? 'Select your design file' : 'Design uploaded'}
-      </label>
-      <input
-        accept="image/*"
-        onChange={handleChange}
-        id="upload"
-        className="w-full rounded-md border border-slate-tr p-1 bg-bg-tertiary text-txt-primary"
-        type="file"
-      />
-    </div>
+    <div className={cn('relative flex flex-col items-center justify-center h-full gap-2 px-2 w-full my-2')}>
+      <div
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={(e) => handleDrop(e)}
+        className='w-full h-full bg-bg-secondary bg-opacity-20 border-4 border-dashed border-slate-tr rounded-md p-2 flex flex-col items-center justify-center gap-2'>
+        <div className='flex flex-col items-center justify-center gap-2'>
+          <p className='text-txt-primary text-center text-lg'>Drag and drop {!selected ? 'your design file here' : 'to choose a different image'}</p>
+          {!selected && <UploadIcon className='w-10 h-10' />}
+          <button type='button' onClick={handleButtonClick} className='bg-bg-secondary bg-opacity-20 border-2 border-accent  rounded-md p-2'>
+            <p className='text-txt-primary'>Or click to select a file</p>
+          </button>
+        </div>
+        <input
+          type="file"
+          id="files"
+          name="files"
+          multiple
+          accept={typeString}
+          ref={fileInputRef}
+          className="hidden"
+          onChange={handleChange}
+          onClick={(event) => {
+            // Reset the input value to allow selecting the same file again
+            (event.target as HTMLInputElement).value = '';
+          }}
+        />
+      </div>
+    </ div>
   )
 }
 
