4ccfac1ecb3951613fd0e0abfea0d14546b7793c
Jordan Roberts <jrobertswebdev@gmail.com>
2025-08-13 13:19:53 -0500

    change to prod server on fly

--- lib/ws.ts
+++ lib/ws.ts
@@ -1,5 +1,5 @@
 // export const WS_URL = 'wss://shark-app-f4rt4.ondigitalocean.app/'
 // export const WS_URL = 'wss://dogmeat.ink-dev.com/ws'
-export const WS_URL = 'wss://mj-backend-staging.fly.dev';
+// export const WS_URL = 'wss://mj-backend-staging.fly.dev';
 // export const WS_URL = 'wss://localhost:8888/ws'
-// export const WS_URL = 'wss://mj-backend.fly.dev';
+export const WS_URL = 'wss://mj-backend.fly.dev';
